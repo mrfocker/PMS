7575a802b30442dfc29d7e5d892ba6980e75eea8
mrfocker <13457329@qq.com>
2017-06-27 23:39:35 +0800

    0627

---
+++ PMS_uestc/src/cn/pms/ssm/mapper/PaperMapper.java
@@ -0,0 +1,12 @@
+package cn.pms.ssm.mapper;
+/** 
+ * <p>Title: </p> 
+ * <p>Description:TODO </p> 
+ * <p>Company: uestc_xr</p> 
+ * @author:  liuxiang 
+ * @date:2017年6月27日
+ * @version 1.0 
+*/
+public class PaperMapper {
+
+}
